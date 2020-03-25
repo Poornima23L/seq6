252dc1c52a723bac0bb0a34bc01dcf505eff1f92
Vinay Kumar Muniraju <poonima23l@gmail.com>
2020-03-25 23:52:54 +0530

    message

---
+++ src/com/tgt/ignite/plus/seq6.java
@@ -0,0 +1,21 @@
+package com.tgt.ignite.plus;
+
+import java.util.Scanner;
+
+public class seq6<i, n> {
+    int  x=1,i,n=10;
+   // Scanner in =new Scanner(System.in);
+    //System.out.println("enter n:");
+    //n=in.nextInt();
+
+    for(i=1;i<=n; i++){
+        System.out.println(x+ " ");
+        if(i%3!=0){
+            x+=4*i;
+        }
+    }
+
+
+
+    
+}
